17c2a7e68ffddd2b8838b2a50ed71248b777c2e5
enzo pedrica <enzo.pedrica@lineabr.local>
2025-11-05 11:03:42 -0300

    Porta dinamica

--- backend/app/main.py
+++ backend/app/main.py
@@ -180,4 +180,6 @@ async def generate_pdf(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
